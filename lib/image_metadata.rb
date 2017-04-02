ab4a002424d735e36d3ddef28a8bc51ae6fd646e
Alex Taylor <alexmctaylorpants@gmail.com>
2017-04-02 09:58:45 -0700

    Implement ImageMetadata for reading metadata

---
+++ lib/image_metadata.rb
@@ -0,0 +1,27 @@
+require 'exif'
+require 'byebug'
+
+class ImageMetadata
+  attr_reader :exif_data
+
+  class ImageNotReadable < StandardError; end
+
+  def initialize(filename)
+    @exif_data = Exif::Data.new(filename)
+
+  # the Exif gem raises an unfortunately generic
+  # RuntimeError when a file is not readable, so
+  # let's make sure we're only rescuing what we need to.
+  rescue RuntimeError => e
+    case e.message
+    when /no EXIF data/
+      raise ImageNotReadable, "Can't extract metadata from file: #{filename}"
+    else
+      raise
+    end
+  end
+
+  def [](key)
+    exif_data[key.to_sym]
+  end
+end
---
+++ spec/lib/image_metadata_spec.rb
@@ -0,0 +1,23 @@
+describe ImageMetadata do
+  let(:image_with_metadata) { File.expand_path('../../sample/image_with_metadata.jpg', __FILE__) }
+  let(:image_with_corrupted_data) { File.expand_path('../../sample/image_with_corrupted_data.jpg', __FILE__) }
+
+  let(:image) { ImageMetadata.new(image_with_metadata) }
+  let(:broken_image) { ImageMetadata.new(image_with_corrupted_data) }
+
+  it 'extracts the specified fields from the image metadata' do
+    expect(image[:gps_latitude]).to eq 38.4
+  end
+
+  it 'works with strings as keys' do
+    expect(image['gps_latitude']).to eq 38.4
+  end
+
+  it 'returns nil when the specified fields are not present' do
+    expect(image[:nuclear_launch_codes]).to eq nil
+  end
+
+  it 'raises an ImageNotReadable error when the image cannot be read' do
+    expect { broken_image['anything'] }.to raise_error ImageMetadata::ImageNotReadable
+  end
+end
